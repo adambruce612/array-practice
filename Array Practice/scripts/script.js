97c8d2fcb5a168d052a844586a24d4ae05b727f8
adambruce612 <93301673+adambruce612@users.noreply.github.com>
2023-02-28 09:46:28 -0600

    Add files via upload

---
+++ Array Practice/scripts/script.js
@@ -0,0 +1,8 @@
+const animals = ['pigs', 'goats', 'sheep'];
+console.log(animals);
+animals.push('dogs');
+console.log(animals);
+
+for (let i = 0; i < animals.length; i++) {
+    console.log(animals[i]);
+}
